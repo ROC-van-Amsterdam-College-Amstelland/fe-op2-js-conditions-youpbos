c77940a2de9e54af1799ff04cd689be1bb0c1f34
David van Oudheusden <40887850+MeesterDaaf@users.noreply.github.com>
2019-11-13 10:39:42 +0100

    Initial commit

---
+++ opdracht1/script.js
@@ -0,0 +1,25 @@
+// A) maak 2 verschillende variabelen met twee willekeurige integers (zelf kiezen)
+
+// jouw code hierondeer-->> 
+var ;
+var ;
+
+// B) toon beide getallen op het scherm met document.getElementById('<jouw_gekozen_id>').innerHTML
+
+// jouw code hieronder-->>
+document.getElementById
+
+// C) Vraag de gebruiker om een INPUT waarde door prompt() te gebruiken. 
+// Gebruik de prompt()-waarde om een variabelen te 'vullen'
+
+// jouw code hieronder-->>
+var ;
+var ;
+
+// D) Vergelijk de twee getallen met elkaar door gebruik te maken van conditions  if()
+
+// jouw code hieronder-->>
+if () {
+
+}
+
